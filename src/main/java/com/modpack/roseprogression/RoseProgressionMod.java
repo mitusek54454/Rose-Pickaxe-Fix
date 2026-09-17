e586d69504408f15e6954f7df0cd9759a8f80fee
mitusek54454 <pabel.bartuzelek@gmail.com>
2026-09-17 17:57:27 +0200

    Update RoseProgressionMod.java

--- src/main/java/com/modpack/roseprogression/RoseProgressionMod.java
+++ src/main/java/com/modpack/roseprogression/RoseProgressionMod.java
@@ -2,8 +2,10 @@ package com.modpack.roseprogression;
 
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.component.Tool;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -19,22 +21,22 @@ public class RoseProgressionMod {
     }
 
     private void modifyDefaultComponents(ModifyDefaultComponentsEvent event) {
-        for (Item item : BuiltInRegistries.ITEM) {
-            var id = BuiltInRegistries.ITEM.getKey(item);
-            if (id != null && "progression_reborn".equals(id.getNamespace()) && "rose_pickaxe".equals(id.getPath())) {
-                event.modify(item, builder -> {
-                    Tool roseTool = new Tool(
-                            List.of(
-                                    Tool.Rule.deniesDrops(BlockTags.NEEDS_DIAMOND_TOOL),
-                                    Tool.Rule.minesAndDrops(BlockTags.MINEABLE_WITH_PICKAXE, 7.0F)
-                            ),
-                            1.0F,
-                            1
-                    );
-                    builder.set(DataComponents.TOOL, roseTool);
-                });
-                break;
-            }
+        // Bezpieczne pobranie kilofa bez uzycia nieistniejacej metody getKey()
+        ResourceLocation rosePickaxeId = ResourceLocation.fromNamespaceAndPath("progression_reborn", "rose_pickaxe");
+        Item rosePickaxe = BuiltInRegistries.ITEM.get(rosePickaxeId);
+
+        if (rosePickaxe != null && rosePickaxe != Items.AIR) {
+            event.modify(rosePickaxe, builder -> {
+                Tool roseTool = new Tool(
+                        List.of(
+                                Tool.Rule.deniesDrops(BlockTags.NEEDS_DIAMOND_TOOL),
+                                Tool.Rule.minesAndDrops(BlockTags.MINEABLE_WITH_PICKAXE, 7.0F)
+                        ),
+                        1.0F,
+                        1
+                );
+                builder.set(DataComponents.TOOL, roseTool);
+            });
         }
     }
 }
