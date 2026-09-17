21896000b3d0984d91962d1a545b939d2a1c4e6f
mitusek54454 <pabel.bartuzelek@gmail.com>
2026-09-17 17:27:32 +0200

    Update RoseProgressionMod.java

--- src/main/java/com/modpack/roseprogression/RoseProgressionMod.java
+++ src/main/java/com/modpack/roseprogression/RoseProgressionMod.java
@@ -2,50 +2,39 @@ package com.modpack.roseprogression;
 
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.component.Tool;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.ModifyDefaultComponentsEvent;
 
 import java.util.List;
 
-@Mod(RoseProgressionMod.MOD_ID)
+@Mod("rose_progression")
 public class RoseProgressionMod {
-    public static final String MOD_ID = "rose_progression";
-
-    public static final TagKey<Block> INCORRECT_FOR_ROSE_TOOL = TagKey.create(
-            Registries.BLOCK,
-            ResourceLocation.fromNamespaceAndPath(MOD_ID, "incorrect_for_rose_tool")
-    );
 
     public RoseProgressionMod(IEventBus modEventBus) {
         modEventBus.addListener(this::modifyDefaultComponents);
     }
 
     private void modifyDefaultComponents(ModifyDefaultComponentsEvent event) {
-        Item rosePickaxe = BuiltInRegistries.ITEM.get(
-                ResourceLocation.fromNamespaceAndPath("progression_reborn", "rose_pickaxe")
-        );
-
-        if (rosePickaxe != Items.AIR) {
-            event.modify(rosePickaxe, builder -> {
-                Tool roseTool = new Tool(
-                        List.of(
-                                Tool.Rule.deniesDrops(INCORRECT_FOR_ROSE_TOOL),
-                                Tool.Rule.minesAndDrops(BlockTags.MINEABLE_WITH_PICKAXE, 7.0F)
-                        ),
-                        1.0F,
-                        1
-                );
-                builder.set(DataComponents.TOOL, roseTool);
-            });
+        for (Item item : BuiltInRegistries.ITEM) {
+            var id = BuiltInRegistries.ITEM.getKey(item);
+            if (id != null && "progression_reborn".equals(id.getNamespace()) && "rose_pickaxe".equals(id.getPath())) {
+                event.modify(item, builder -> {
+                    Tool roseTool = new Tool(
+                            List.of(
+                                    Tool.Rule.deniesDrops(BlockTags.NEEDS_DIAMOND_TOOL),
+                                    Tool.Rule.minesAndDrops(BlockTags.MINEABLE_WITH_PICKAXE, 7.0F)
+                            ),
+                            1.0F,
+                            1
+                    );
+                    builder.set(DataComponents.TOOL, roseTool);
+                });
+                break;
+            }
         }
     }
 }
